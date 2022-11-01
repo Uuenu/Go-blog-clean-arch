766ce9d75add6ff37aa57f535dc389b0c6f479db
Uuenu <nelsonff918@gmail.com>
2022-11-01 20:57:11 +0300

    add mongodb client

--- pkg/client/mongodb/mongodb.go
+++ pkg/client/mongodb/mongodb.go
@@ -2,11 +2,40 @@ package mongodb
 
 import (
 	"context"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+//TODO Chose best way to return *mongo.Client or *mongo.Database
 func NewClient(ctx context.Context, host, port, username, password, database, authDB string) (*mongo.Client, error) {
+	var mongodbURL string
+	var isAuth bool
+	if username == "" && password == "" {
+		mongodbURL = fmt.Sprintf("mongodb://%s:%s", host, port)
+	} else {
+		isAuth = true
+		mongodbURL = fmt.Sprintf("mongodb://%s:%s@%s:%s", username, password, host, port)
+	}
 
-	return nil, nil
+	clientOptions := options.Client().ApplyURI(mongodbURL)
+	if isAuth {
+		if authDB == "" {
+			authDB = database
+		}
+		clientOptions.SetAuth(options.Credential{})
+	}
+
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to mongodb due to error:%v", err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		return nil, fmt.Errorf("failed to ping mongodb due to error:%v", err)
+	}
+
+	return client, nil
+
 }
